e0b26b02f37ea55742ad484c68b7a08f2008f8a5
Krtmrls <moraleskurty35@gmail.com>
2026-02-11 20:24:42 +0800

    Feat Added Subtraction function

---
+++ Ordiz_github_calculator_sample/Subtraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordiz_github_calculator_sample
+{
+    public class Subtraction
+    {
+        public double Sub(double num1, double num2)
+        {
+            double num = num1 - num2;
+
+            return num;
+
+
+
+            
+        }
+
+    }
+}
+
