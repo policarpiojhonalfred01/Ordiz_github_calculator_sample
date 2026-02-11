9c23cf65c3fdef6a3772f53705ce02630b491b0f
glenneduardordiz22-bit <glenneduardordiz22@gmail.com>
2026-02-11 20:21:14 +0800

    Feat: Add Division func

---
+++ Ordiz_github_calculator_sample/Division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordiz_github_calculator_sample
+{
+    public class Division
+    {
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return num1 / num2;
+        }
+
+    }
+}
